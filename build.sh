3e56315747ffdfe481ade55686bf7b2760100dd5
Søren Lund <slu@localhost.localdomain>
2025-06-14 01:29:57 +0200

    fix(date): fix date calc to work in POSIX sh

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ emacs -Q --script build.el
 base_url="${BASE_URL:-http://localhost:8080}"
 
 written_date() {
-    local date=$1
-    local month month_fmt
-
+    date="$1"
     month=$(date -d"$date" +"%-m")
 
     if [ "$month" -ge 3 ] && [ "$month" -le 7 ]; then
@@ -17,7 +15,12 @@ written_date() {
     else
         month_fmt="%b." # Abbriviated name (Jan., Feb., Aug., Sep., Oct., Nov., Dec.)
     fi
-    echo -n "$(date -d "$date" +"%A, ${month_fmt} %-e, %Y")"
+    printf "%s" "$(date -d "$date" +"%A, ${month_fmt} %-e, %Y")"
+}
+
+get_mod_date() {
+    file="$1"
+    git log -1 --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%ad' "$file"
 }
 
 for filepath in public/*.html; do
@@ -25,7 +28,7 @@ for filepath in public/*.html; do
     org_path="content/${filename%html}org"
     home_nav="🏠<a href='${base_url}'>Home</a>"
     this_url="🌐<a href='${base_url}/${filename}'>${base_url}/${filename}</a>"
-    mod_date="📅$(written_date "$(git log -1 --pretty="format:%ci" "$org_path")")"
+    mod_date="📅$(written_date "$(get_mod_date "$org_path")")"
 
     byline="<p class='byline'><span>${home_nav}</span><span>${mod_date}</span></p>"
     footer="<span>Content ©2025 Søren Lund and licensed under <a href='https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1'>CC BY-SA 4.0</a></span><span>${this_url}</span>"
