e5db6af519ba8cbf7b9615ed2a6e8cf205cc55cf
Søren Lund <zqrl@novonordisk.com>
2026-02-06 08:40:29 +0100

    feat(build): add custom html preamble and postamble functions
    - Define functions for html preamble and postamble formatting
    - Update project alist to use new formatting functions
    - Enhance html head styles for improved navigation appearance

--- build.el
+++ build.el
@@ -1,6 +1,14 @@
 ;; Load the publishing systems
 (require 'ox-publish)
 
+(defun my/org-site-html-preamble-format ()
+  "Return the html preamble format alist for org-publish."
+  '(("en" "<ul><li><a href='index.html'>Home</a></li><li><a href='perl.html'>Perl</a></li></ul><hr>")))
+
+(defun my/org-site-html-postamble-format ()
+  "Return the html preamble format alist for org-publish."
+  '(("en" "<hr><p><em>Copyright ©2025-2026 Søren Lund and licensed under <a href='https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1'>CC BY-SA 4.0</a></em></p><hr><p><a href=\"https://codeberg.org/slu/slu.codeberg.page\"><img alt=\"Get it on Codeberg\" src=\"https://get-it-on.codeberg.org/get-it-on-blue-on-white.svg\" height=\"30\"></a></p>")))
+
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
@@ -15,8 +23,10 @@
              :with-toc t                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
              :time-stamp-file nil       ;; Don't include time stamp in file
+             :html-preamble t           ;; Include header
+             :html-preamble-format (my/org-site-html-preamble-format)
              :html-postamble t          ;; Include footer
-             :html-postamble-format '(("en" "<hr><p><em>Copyright ©2025 Søren Lund and licensed under <a href='https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1'>CC BY-SA 4.0</a></em></p><hr><p><a href=\"https://codeberg.org/slu/slu.codeberg.page\"><img alt=\"Get it on Codeberg\" src=\"https://get-it-on.codeberg.org/get-it-on-blue-on-white.svg\" height=\"30\"></a></p>")))
+             :html-postamble-format (my/org-site-html-postamble-format))
 
        (list "org-site:static"
              :recursive t
@@ -29,7 +39,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<style>html {margin:2ch;} body {max-width:70ch;padding:2ch;margin:auto;}</style>")
+      org-html-head "<style>html {margin:2ch;} body {max-width:70ch;padding:2ch;margin:auto;} #preamble ul {margin:0;padding:0;} #preamble li {display:inline-block;margin:0 .25em;;border:1px solid goldenrod;padding:.25em;background:lightgoldenrodyellow;}</style>")
 
 ;; Generate the site output
 (org-publish-all t)
